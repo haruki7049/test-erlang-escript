d8870a23aba770ba16339986e32c24303c4e54c7
haruki7049 <tontonkirikiri@gmail.com>
2025-12-20 17:52:04 +0900

    Update

---
+++ src/server_app.erl
@@ -0,0 +1,23 @@
+%% @private
+-module(server_app).
+-behaviour(application).
+
+%% API.
+-export([start/2]).
+-export([stop/1]).
+
+%% API.
+
+start(_Type, _Args) ->
+	Dispatch = cowboy_router:compile([
+		{'_', [
+			{"/", toppage, []}
+		]}
+	]),
+	{ok, _} = cowboy:start_clear(http, [{port, 8080}], #{
+		env => #{dispatch => Dispatch}
+	}),
+	hello_world_sup:start_link().
+
+stop(_State) ->
+	ok = cowboy:stop_listener(http).
