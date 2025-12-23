c74ee7ccecf1a81d0ee5f45d290f73d89891ceaf
haruki7049 <tontonkirikiri@gmail.com>
2025-12-23 15:48:59 +0900

    Update

--- src/bin/test_erlang_escript.erl
+++
@@ -1,7 +0,0 @@
--module(test_erlang_escript).
--export([main/1]).
-
-%% Entry point for the escript
-main(_Args) ->
-    {ok, _} = application:ensure_all_started(test_erlang_escript),
-    timer:sleep(infinity).
