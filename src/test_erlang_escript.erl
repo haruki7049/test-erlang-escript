0f43e797c71fa32a0d45f1aeaade49d251648792
haruki7049 <tontonkirikiri@gmail.com>
2025-12-23 22:58:44 +0900

    Update

--- src/test_erlang_escript.erl
+++ src/test_erlang_escript.erl
@@ -4,7 +4,7 @@
 
 main(_Args) ->
     {ok, _} = application:ensure_all_started(cowboy),
-    {ok, Pid} = application:ensure_all_started(server),
-    io:format("Started: ~w~n", [Pid]),
+    {ok, _Started} = application:ensure_all_started(server),
+    io:format("Server started at http://localhost:8080~n", []),
 
     timer:sleep(infinity).
