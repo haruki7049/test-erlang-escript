2b23482059fa9d89e3f530706943c7b4e582a6c5
haruki7049 <tontonkirikiri@gmail.com>
2025-12-23 18:35:54 +0900

    Update

--- src/bin/test_erlang_escript.erl
+++ src/bin/test_erlang_escript.erl
@@ -2,7 +2,7 @@
 -export([main/1]).
 
 main(_Args) ->
-    {ok, Pid} = application:ensure_all_started(server_app),
+    {ok, Pid} = application:ensure_all_started(test_erlang_escript),
     io:format("Started applications: ~p~n", [Pid]),
     timer:sleep(infinity).
 
