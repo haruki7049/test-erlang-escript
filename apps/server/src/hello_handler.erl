3032a3784d20cb6da523e207e6b54acbf4fb1062
haruki7049 <tontonkirikiri@gmail.com>
2025-12-23 20:40:50 +0900

    style: nix fmt

--- apps/server/src/hello_handler.erl
+++ apps/server/src/hello_handler.erl
@@ -3,10 +3,11 @@
 
 -export([init/2]).
 
+
 %% Handle incoming HTTP requests
 init(Req0, State) ->
     Req = cowboy_req:reply(200,
-        #{<<"content-type">> => <<"text/plain">>},
-        <<"Hello, Erlang World!">>,
-        Req0),
+                           #{<<"content-type">> => <<"text/plain">>},
+                           <<"Hello, Erlang World!">>,
+                           Req0),
     {ok, Req, State}.
--- src/test_erlang_escript.erl
+++ src/test_erlang_escript.erl
@@ -1,6 +1,7 @@
 -module(test_erlang_escript).
 -export([main/1]).
 
+
 main(_Args) ->
     {ok, _} = application:ensure_all_started(cowboy),
     {ok, Pid} = application:ensure_all_started(server),
